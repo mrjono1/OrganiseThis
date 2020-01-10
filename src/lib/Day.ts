9e75fb6887279534159c2b28a9a24d65c841791a
mrjono1 <jonoclarnette@gmail.com>
2020-01-10 14:59:58 +1100

    organise code a bit

--- src/lib/Day.ts
+++ src/lib/Day.ts
@@ -1,6 +1,5 @@
-﻿import { Span, Room } from 'lib';
-import { Settings, DaySetting, PersonSetting } from 'settings';
-import { randomIndexAndItem } from 'helpers';
+﻿import { Room } from 'lib';
+import { Settings, DaySetting } from 'settings';
 import { Weekday } from 'types';
 
 export class Day {
@@ -26,95 +25,11 @@ export class Day {
       return;
     }
 
+    // Create the Rooms in the day
     for (const dayRoomSetting of this.daySetting.dayRoomSettings) {
-      const room = new Room(this.settings.idCounter++, this.settings, dayRoomSetting);
-      for (const spanSetting of dayRoomSetting.spanSettings) {
-        let personSettingItem: PersonSetting | undefined = undefined;
-        let personSettingIndex: number | undefined = undefined;
-
-        const { fixedPersonIndex, fixedPersonItem } = this.fixedPerson(spanSetting.id);
-        personSettingItem = fixedPersonItem;
-        personSettingIndex = fixedPersonIndex;
-
-        if (personSettingItem === undefined) {
-          // todo ensure unique list
-          const usedIndexes = this.getUnavailablePersonIndexes(spanSetting.id).concat(personIndexesUsed);
-
-          // Filter out people who dont have the skill
-          if (
-            spanSetting.skillSettingIds &&
-            spanSetting.skillSettingIds.length !== 0 &&
-            this.settings.skillSettings &&
-            this.settings.skillSettings.length !== 0
-          ) {
-            for (let index = 0; index < this.settings.personSettings.length; index++) {
-              if (usedIndexes.includes(index)) {
-                continue;
-              }
-              let matched = false;
-              const personSetting = this.settings.personSettings[index];
-              if (personSetting.skillSettingIds && personSetting.skillSettingIds.length !== 0) {
-                let matchedSkillCount = 0;
-                for (const spanSkillId of spanSetting.skillSettingIds) {
-                  if (personSetting.skillSettingIds.includes(spanSkillId)) {
-                    matchedSkillCount++;
-                  }
-                }
-                matched = matchedSkillCount === spanSetting.skillSettingIds.length;
-              }
-              if (!matched) {
-                usedIndexes.push(index);
-              }
-            }
-          }
-
-          if (this.settings.personSettings.length !== usedIndexes.length) {
-            const { index, item } = randomIndexAndItem(this.settings.personSettings, usedIndexes);
-            personSettingItem = item;
-            personSettingIndex = index;
-          }
-        }
-
-        const span = new Span(this.settings.idCounter++, this.settings, spanSetting, personSettingItem);
-        room.spans.push(span);
-
-        if (personSettingIndex !== undefined) {
-          personIndexesUsed.push(personSettingIndex);
-        }
-      }
-
+      const room = new Room(this.settings.idCounter++, this.settings, dayRoomSetting, personIndexesUsed);
       this.rooms.push(room);
     }
-  }
-
-  private fixedPerson(spanSettingId: number): { fixedPersonIndex?: number; fixedPersonItem?: PersonSetting } {
-    for (let index = 0; index < this.settings.personSettings.length; index++) {
-      const personSetting = this.settings.personSettings[index];
-      if (!personSetting.fixedShiftsIds) {
-        continue;
-      }
-      const matched = personSetting.fixedShiftsIds.find((id: number) => id === spanSettingId);
-      if (matched) {
-        return { fixedPersonIndex: index, fixedPersonItem: personSetting };
-      }
-    }
-    return { fixedPersonIndex: undefined, fixedPersonItem: undefined };
-  }
-
-  private getUnavailablePersonIndexes(spanSettingId: number): number[] {
-    const indexes: number[] = [];
-    for (let index = 0; index < this.settings.personSettings.length; index++) {
-      const personSetting = this.settings.personSettings[index];
-      if (!personSetting.unavailableShiftIds || personSetting.unavailableShiftIds.length === 0) {
-        continue;
-      }
-      for (const id of personSetting.unavailableShiftIds) {
-        if (id === spanSettingId) {
-          indexes.push(id);
-        }
-      }
-    }
-    return indexes;
   }
 
   evaluate(): void {
--- src/lib/Room.ts
+++ src/lib/Room.ts
@@ -1,5 +1,6 @@
-﻿import { Settings, DayRoomSetting } from 'settings';
+﻿import { Settings, DayRoomSetting, PersonSetting, SpanSetting } from 'settings';
 import { Span } from 'lib';
+import { randomIndexAndItem } from 'helpers';
 
 export class Room {
   public readonly id: number;
@@ -9,16 +10,106 @@ export class Room {
 
   private _fitness?: number;
 
-  constructor(id: number, settings: Settings, dayRoomSetting: DayRoomSetting) {
+  constructor(id: number, settings: Settings, dayRoomSetting: DayRoomSetting, personIndexesUsed?: number[]) {
     this.id = id;
     this.settings = settings;
     this.dayRoomSetting = dayRoomSetting;
     this.spans = [];
-    this.newRoom();
+    this.newRoom(personIndexesUsed ?? []);
   }
 
-  private newRoom(): void {
-    // todo in future span settings will influence this section
+  private newRoom(personIndexesUsed: number[]): void {
+    // Create the Spans of the Room
+    for (const spanSetting of this.dayRoomSetting.spanSettings) {
+      const { index, item } = this.getPerson(spanSetting, personIndexesUsed);
+
+      const span = new Span(this.settings.idCounter++, this.settings, spanSetting, item);
+      this.spans.push(span);
+
+      if (index !== undefined) {
+        personIndexesUsed.push(index);
+      }
+    }
+  }
+
+  private getPerson(spanSetting: SpanSetting, personIndexesUsed: number[]): { item?: PersonSetting; index?: number } {
+    let personSettingItem: PersonSetting | undefined = undefined;
+    let personSettingIndex: number | undefined = undefined;
+
+    const { fixedPersonIndex, fixedPersonItem } = this.fixedPerson(spanSetting.id);
+    personSettingItem = fixedPersonItem;
+    personSettingIndex = fixedPersonIndex;
+
+    if (personSettingItem === undefined) {
+      // todo ensure unique list
+      const usedIndexes = this.getUnavailablePersonIndexes(spanSetting.id).concat(personIndexesUsed);
+
+      // Filter out people who dont have the skill
+      if (
+        spanSetting.skillSettingIds &&
+        spanSetting.skillSettingIds.length !== 0 &&
+        this.settings.skillSettings &&
+        this.settings.skillSettings.length !== 0
+      ) {
+        for (let index = 0; index < this.settings.personSettings.length; index++) {
+          if (usedIndexes.includes(index)) {
+            continue;
+          }
+          let matched = false;
+          const personSetting = this.settings.personSettings[index];
+          if (personSetting.skillSettingIds && personSetting.skillSettingIds.length !== 0) {
+            let matchedSkillCount = 0;
+            for (const spanSkillId of spanSetting.skillSettingIds) {
+              if (personSetting.skillSettingIds.includes(spanSkillId)) {
+                matchedSkillCount++;
+              }
+            }
+            matched = matchedSkillCount === spanSetting.skillSettingIds.length;
+          }
+          if (!matched) {
+            usedIndexes.push(index);
+          }
+        }
+      }
+
+      if (this.settings.personSettings.length !== usedIndexes.length) {
+        const { index, item } = randomIndexAndItem(this.settings.personSettings, usedIndexes);
+        personSettingItem = item;
+        personSettingIndex = index;
+      }
+    }
+
+    return { index: personSettingIndex, item: personSettingItem };
+  }
+
+  private fixedPerson(spanSettingId: number): { fixedPersonIndex?: number; fixedPersonItem?: PersonSetting } {
+    for (let index = 0; index < this.settings.personSettings.length; index++) {
+      const personSetting = this.settings.personSettings[index];
+      if (!personSetting.fixedShiftsIds) {
+        continue;
+      }
+      const matched = personSetting.fixedShiftsIds.find((id: number) => id === spanSettingId);
+      if (matched) {
+        return { fixedPersonIndex: index, fixedPersonItem: personSetting };
+      }
+    }
+    return { fixedPersonIndex: undefined, fixedPersonItem: undefined };
+  }
+
+  private getUnavailablePersonIndexes(spanSettingId: number): number[] {
+    const indexes: number[] = [];
+    for (let index = 0; index < this.settings.personSettings.length; index++) {
+      const personSetting = this.settings.personSettings[index];
+      if (!personSetting.unavailableShiftIds || personSetting.unavailableShiftIds.length === 0) {
+        continue;
+      }
+      for (const id of personSetting.unavailableShiftIds) {
+        if (id === spanSettingId) {
+          indexes.push(id);
+        }
+      }
+    }
+    return indexes;
   }
 
   get fitness(): number {
